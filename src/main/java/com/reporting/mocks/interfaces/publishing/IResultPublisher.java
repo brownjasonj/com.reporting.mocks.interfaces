9203fa5435c156b012d4467cf8c16e4e394431d9
Jason Brown <jasonbrown@fastmail.fm>
2019-05-01 23:17:20 +0200

    code changes to remove build warnings.  updated version to be in sync with mocks.model

--- src/main/java/com/reporting/mocks/interfaces/publishing/IResultPublisher.java
+++ src/main/java/com/reporting/mocks/interfaces/publishing/IResultPublisher.java
@@ -1,12 +1,13 @@
 package com.reporting.mocks.interfaces.publishing;
 
 import com.reporting.mocks.model.*;
+import com.reporting.mocks.model.risks.Risk;
 
 public interface IResultPublisher {
     void publish(CalculationContext calculationContext);
     void publish(MarketEnv marketEnv);
     void publishIntradayRiskResultSet(RiskResultSet riskResultSet);
-    void publishIntradayRiskResult(RiskResult riskResult);
+    void publishIntradayRiskResult(RiskResult<? extends Risk> riskResult);
     void publishIntradayTrade(TradeLifecycle tradeLifecycle);
     void publishEndofDayRiskRun(RiskResultSet riskResultSet);
 }
